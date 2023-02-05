192a43d7dce06a5a8b2ea78ea13ff8cdc54b465b
jeonga-Son <jjeong0106@gmail.com>
2023-02-05 17:47:27 +0900

    Call by reference test

--- mission/src/mission/basic/ArrayExam.java
+++ mission/src/mission/basic/ArrayExam.java
@@ -3,8 +3,16 @@ package mission.basic;
 public class ArrayExam {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		int[] arr = {1,2,3};
+		int[] brr = arr;
+		
+		System.out.println("arr 주소값 : " + arr);
+		System.out.println("brr 주소값 : " + brr);
+		
+		brr[0] = 5;
+		
+		System.out.println("arr[0] : " + arr[0]);
+		System.out.println("brr[0] : " + brr[0]);
 	}
 
 }
